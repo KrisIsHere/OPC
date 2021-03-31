050068d25ef42a2b340bef307f0104ed0918d24d
KrisIsHere <73352269+KrisIsHere@users.noreply.github.com>
2021-03-31 23:05:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,37 +6,6 @@ CM.copy("info", "tools")
 os.chdir("tools")
 
 def win():
-        def chat():
-            nickname = input("Choose your \033[0;31mnickname\033[97;40m: ")
-            server = input("Enter \033[92;40mserver \033[0;31mIP\033[97;40m: ")
-
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect((server, 14900))
-
-            def receive():
-                while True:
-                    try:
-                        message = client.recv(1024).decode('ascii')
-                        if message == 'NICK':
-                            client.send(nickname.encode('ascii'))
-                        else:
-                            print(message)
-                    except:
-                        print("An error occured!")
-                        client.close()
-                        break
-
-            def write():
-                while True:
-                    message = '\033[0;31m{}\033[97;40m:\033[92;40m {}\033[97;40m'.format(nickname, input(''))
-                    client.send(message.encode('ascii'))
-
-            receive_thread = threading.Thread(target=receive)
-            receive_thread.start()
-
-            write_thread = threading.Thread(target=write)
-            write_thread.start()
-
         def main():
             CM.clearscreen()
             print("OPC 0.4 | Coded by \033[0;31mKrisIsHere\033[97;40m & \033[0;31mOwenwastaken\033[97;40m")
@@ -110,36 +79,6 @@ def win():
 
 
 def linux():
-    def chat():
-            nickname = input("Choose your \033[0;31mnickname\033[97;40m: ")
-            server = input("Enter \033[92;40mserver \033[0;31mIP\033[97;40m: ")
-
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect((server, 14900))
-
-            def receive():
-                while True:
-                    try:
-                        message = client.recv(1024).decode('ascii')
-                        if message == 'NICK':
-                            client.send(nickname.encode('ascii'))
-                        else:
-                            print(message)
-                    except:
-                        print("An error occured!")
-                        client.close()
-                        break
-
-            def write():
-                while True:
-                    message = '\033[0;31m{}\033[97;40m:\033[92;40m {}\033[97;40m'.format(nickname, input(''))
-                    client.send(message.encode('ascii'))
-
-            receive_thread = threading.Thread(target=receive)
-            receive_thread.start()
-
-            write_thread = threading.Thread(target=write)
-            write_thread.start()
     def main():
         os.system("clear")
         print("OPC 0.4 | Coded by \033[0;31mKrisIsHere\033[97;40m & \033[0;31mOwenwastaken\033[97;40m")
