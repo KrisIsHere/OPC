98c9fd6a7e8b8190018ba48964e94dbfc0f6c82d
KrisIsHere <73352269+KrisIsHere@users.noreply.github.com>
2021-04-01 11:51:52 +0300

    Update client.py

--- tools/client.py
+++ tools/client.py
@@ -1,37 +1,31 @@
-import socket
-import threading
-import os
-from sys import platform
+import threading,socket
 
-def chat():
-        nickname = input("Choose your \033[0;31mnickname\033[97;40m: ")
-        server = input("Enter \033[92;40mserver \033[0;31mIP\033[97;40m: ")
+nickname = input("Choose your \033[0;31mnickname\033[97;40m: ")
+server = input("Enter \033[92;40mserver \033[0;31mIP\033[97;40m: ")
 
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((server, 14900))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server, 14900))
 
-        def receive():
-            while True:
-                try:
-                    message = client.recv(1024).decode('ascii')
-                    if message == 'NICK':
-                        client.send(nickname.encode('ascii'))
-                    else:
-                        print(message)
-                except:
-                    print("An error occured!")
-                    client.close()
-                    break
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occured!")
+            client.close()
+            break
 
-        def write():
-            while True:
-                message = '\033[0;31m{}\033[97;40m:\033[92;40m {}\033[97;40m'.format(nickname, input(''))
-                client.send(message.encode('ascii'))
+def write():
+    while True:
+        message = '\033[0;31m{}\033[97;40m:\033[92;40m {}\033[97;40m'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
 
-        receive_thread = threading.Thread(target=receive)
-        receive_thread.start()
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
 
-        write_thread = threading.Thread(target=write)
-        write_thread.start()
-
-chat()
+write_thread = threading.Thread(target=write)
+write_thread.start()
